212cf0d181f0cb2afa243387d17a580f511ec1f7
OnkarBagwe <onkarbagwe25@gmail.com>
2023-05-22 18:25:54 +0530

    region specific

--- app.py
+++ app.py
@@ -31,24 +31,73 @@ output_file = "/Users/onkar/Desktop/Quantium Virtual Program/quantium-virtual-pr
 combined_df.to_csv(output_file, index=False)
 
 
-# Sort the data by date
-combined_df = combined_df.sort_values('date')
+# # Sort the data by date
+# combined_df = combined_df.sort_values('date')
 
-# Create the line chart
-fig = px.line(combined_df, x='date', y='sales')
+# # Create the line chart
+# fig = px.line(combined_df, x='date', y='sales')
+
+# # Create the Dash app
+# app = Dash(__name__)
+
+# # Set up the layout
+# app.layout = html.Div(children=[
+#     html.H1(children='Sales Visualization'),
+
+#     dcc.Graph(
+#         id='sales-chart',
+#         figure=fig
+#     )
+# ])
+
+# # Run the app
+# if __name__ == '__main__':
+#     app.run_server(debug=True)
+
 
 # Create the Dash app
 app = Dash(__name__)
 
 # Set up the layout
 app.layout = html.Div(children=[
-    html.H1(children='Sales Visualization'),
+    html.H1(children='Sales Visualization', style={'text-align': 'center', 'margin-top': '30px', 'color': '#FF0000'}),
+    html.H2(children='Filter by Region', style={'text-align': 'center', 'margin-top': '20px', 'color': 'blue'}),
+
+    dcc.RadioItems(
+        id='region-filter',
+        options=[
+            {'label': 'North', 'value': 'north'},
+            {'label': 'East', 'value': 'east'},
+            {'label': 'South', 'value': 'south'},
+            {'label': 'West', 'value': 'west'},
+            {'label': 'All', 'value': 'all'}
+        ],
+        value='all',
+        labelStyle={'display': 'inline-block', 'margin': '0 10px'}
+    ),
 
     dcc.Graph(
         id='sales-chart',
-        figure=fig
+        style={'height': '400px', 'margin-top': '30px'}
     )
-])
+], style={'width': '50%', 'margin': 'auto', 'font-family': 'Arial, sans-serif', 'background-color': '#f9f9f9', 'padding': '20px', 'border': '1px solid #ccc'})
+
+# Define the callback function to update the chart based on the region selection
+@app.callback(
+    Output('sales-chart', 'figure'),
+    [Input('region-filter', 'value')]
+)
+def update_chart(region):
+    if region == 'all':
+        df_filtered = combined_df
+    else:
+        df_filtered = combined_df[combined_df['region'] == region]
+
+    df_filtered = df_filtered.sort_values('date')
+
+    fig = px.line(df_filtered, x='date', y='sales')
+
+    return fig
 
 # Run the app
 if __name__ == '__main__':
