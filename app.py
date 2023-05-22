2627dc0d03dc3c077e039df66248f84416e06462
OnkarBagwe <onkarbagwe25@gmail.com>
2023-05-22 14:11:32 +0530

    data cleaning

--- app.py
+++ app.py
@@ -1,3 +1,25 @@
 # Import packages
 from dash import Dash
 import pandas as pd
+import glob
+import os
+
+folder_path = "/Users/onkar/Desktop/Quantium Virtual Program/quantium-virtual-program/data"  # Replace with the actual folder path where the CSV files are located
+file_pattern = "*.csv"
+
+# Construct the full file paths by joining the folder path with the file pattern
+file_paths = glob.glob(os.path.join(folder_path, file_pattern))
+data_frames = []
+
+for file_path in file_paths:
+    df = pd.read_csv(file_path)
+    data_frames.append(df)
+
+combined_df = pd.concat(data_frames, ignore_index=True)
+
+combined_df = combined_df[combined_df['product'] == 'pink morsel']
+
+combined_df['sales'] = combined_df['price'].str.replace('$', '').astype(float) * combined_df['quantity']
+combined_df = combined_df.drop(['product','price', 'quantity'], axis=1)
+
+print(combined_df)
